96674d447826c82cadd8d8ca4962a6a00323acc0
Lia Zadoyan <liazadoyan@gmail.com>
2017-09-12 20:50:06 -0700

    show poster, title, overview in table

---
+++ Flixter/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  Flixter
+//
+//  Created by Lia Zadoyan on 9/12/17.
+//  Copyright © 2017 Lia Zadoyan. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
